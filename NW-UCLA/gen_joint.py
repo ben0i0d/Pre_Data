9778609101c7bb8c82c39846979c16db3810def5
ben0i0d <ben0i0d@foxmail.com>
2024-10-29 05:25:55 +0800

    repair nw-ucla(label strat from 1)

--- NW-UCLA/gen_joint.py
+++ NW-UCLA/gen_joint.py
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 
     train_data = np.array(train_data).transpose(0, 3, 1, 2) # N T V C to N C T V
     val_data = np.array(val_data).transpose(0, 3, 1, 2) # N T V C to N C T V
-    train_label = np.array(train_label)
-    val_label = np.array(val_label)
+    train_label = np.array(train_label) - 1 # label start 1 to 0
+    val_label = np.array(val_label) - 1 # label start 1 to 0
 
     np.save('data/npy/train_joint.npy', train_data)
     np.save('data/npy/val_joint.npy', val_data)
